ef3b2654b474535c4f0bff17f962cfc06cd35e45
ElizabethG578 <egrizzle578@gmail.com>
2022-12-21 23:54:26 +0000

    Saved progress on IfStatement 2022-12-21 23:54:26 +0000

--- BooleanOperators/src/main/java/Expressions.java
+++ BooleanOperators/src/main/java/Expressions.java
@@ -14,7 +14,12 @@ public class Expressions {
      * @return if b is true, return true. if b is false, return false.
      */
     public boolean returnBoolean(boolean b){
+        if (b){
+            return true;
+        }
+        else{
         return false;
+        }
     }
 
     /**
@@ -23,7 +28,11 @@ public class Expressions {
      * look into boolean operations to do this without using if/else statements.
      */
     public boolean returnNot(boolean b){
-        return false;
+        if (b){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     /**

45e99d39a6070eb4ccb703fc39a184dc66800ddc
ElizabethG578 <egrizzle578@gmail.com>
2022-12-21 04:05:19 +0000

    Saved progress on HelloWorld 2022-12-21 04:05:19 +0000

--- HelloWorld/test-output.sh
+++ HelloWorld/test-output.sh
@@ -4,7 +4,7 @@
 -------------------------------------------------------
 Picked up JAVA_TOOL_OPTIONS:  -Xmx3489m
 Running HelloWorldTest
-Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.037 sec
+Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.093 sec
 
 Results :
 

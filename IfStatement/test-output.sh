cf36bc0279d1008d491e5600bdff15c5fc7e1f78
ElizabethG578 <egrizzle578@gmail.com>
2022-12-21 06:09:28 +0000

    Saved progress on IfStatement 2022-12-21 06:09:28 +0000

---
+++ IfStatement/test-output.sh
@@ -0,0 +1,12 @@
+[ERROR] COMPILATION ERROR : 
+[ERROR] /workspace/pep-gitpod-workspace/pep-labs/IfStatement/src/main/java/IfStatement.java:[25,5] missing return statement
+[ERROR] Failed to execute goal org.apache.maven.plugins:maven-compiler-plugin:3.1:compile (default-compile) on project PEPLabsChallenges: Compilation failure
+[ERROR] /workspace/pep-gitpod-workspace/pep-labs/IfStatement/src/main/java/IfStatement.java:[25,5] missing return statement
+[ERROR] 
+[ERROR] -> [Help 1]
+[ERROR] 
+[ERROR] To see the full stack trace of the errors, re-run Maven with the -e switch.
+[ERROR] Re-run Maven using the -X switch to enable full debug logging.
+[ERROR] 
+[ERROR] For more information about the errors and possible solutions, please read the following articles:
+[ERROR] [Help 1] http://cwiki.apache.org/confluence/display/MAVEN/MojoFailureException
